6b715aa091b57fbf1ab2ddcfc4e9331e69a62562
Katona Valentin <djvalee.yt@gmail.com>
2022-04-11 02:03:57 +0200

    first

---
+++ src/app/places/places.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Place } from './place.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+  private _places: Place[] = [
+    new Place(
+      'p1',
+      'Manhattan',
+      'In centre of New York.',
+      'https://www.otptravel.hu/userfiles/USA/NYC-csoportos/NYC-Manhattan-2.jpg',
+      149.99
+    ),
+    new Place(
+      'p2',
+      'California',
+      'Romantic place.',
+      'https://www.artnews.com/wp-content/uploads/2022/03/LA-sunrise-small.jpg?w=1000',
+      149.99
+    ),
+    new Place(
+      'p3',
+      'Paris',
+      'Beautiful city',
+      'https://images.adsttc.com/media/images/5d44/14fa/284d/d1fd/3a00/003d/large_jpg/eiffel-tower-in-paris-151-medium.jpg?1564742900',
+      99.99
+    )
+  ];
+
+  get places() {
+    // eslint-disable-next-line no-underscore-dangle
+    return [...this._places];
+  }
+
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  constructor() { }
+
+  getPlace(id: string) {
+    // eslint-disable-next-line no-underscore-dangle
+    return {...this._places.find(p => p.id === id)};
+  }
+}
